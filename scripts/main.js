0c7be3c4df12dee2aaa5d65b04cb41961c25d5cc
Natalie Villasana <navillasa@gmail.com>
2017-07-20 16:24:54 -0400

    adding submit selector and event handler

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+SUBMIT_SELECTOR = "[data-submit-me]";
+
+function getOrderName () {
+    var orderName = document.getElementById("coffeeOrder");
+    return orderName;
+}
+
+console.log(getOrderName());
+
+var formInputArray = document.getElementsByClassName()
+
+function submitEventHandler() {
+    var buddin = document.querySelector(SUBMIT_SELECTOR);
+}
